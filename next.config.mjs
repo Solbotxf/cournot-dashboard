e7d687b1102d2d7d43406cab349cfb54a9f87783
Ambero <ytu@carv.io>
2026-02-09 16:00:24 +0800

    Add env var mapping

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  env: {
+    PLAYGROUND_PUBLIC_PROTOCOL_API_BASE: process.env.PLAYGROUND_PUBLIC_PROTOCOL_API_BASE,
+  },
 };
 
 export default nextConfig;
